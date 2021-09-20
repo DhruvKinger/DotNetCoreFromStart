6389ec1011c83be4c50332d7f0f12cbf9072da4e
Dhruv Kinger <cse1785@gmail.com>
2021-09-20 22:25:25 +0530

    push

--- Startup.cs
+++ Startup.cs
@@ -20,8 +20,10 @@ namespace DotnetCoreEmptyApplication
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {          
-           
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
         }
     }
 }
