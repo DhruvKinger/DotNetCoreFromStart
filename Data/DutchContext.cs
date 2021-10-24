54d95ef70e2e09b19a73f61710b00f433a6e68dd
Dhruv Kinger <cse1785@gmail.com>
2021-10-25 00:18:11 +0530

    Add New classes

---
+++ Data/DutchContext.cs
@@ -0,0 +1,15 @@
+﻿using DotnetCoreEmptyApplication.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreEmptyApplication.Data
+{
+    public class DutchContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Data/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCoreEmptyApplication.Data.Entities
+{
+  public class Order
+  {
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string OrderNumber { get; set; }
+    public ICollection<OrderItem> Items { get; set; }
+  }
+}
